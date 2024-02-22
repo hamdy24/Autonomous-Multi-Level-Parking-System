877818a2f2de35ac14569634843346de748d5b47
hamdy24 <hamdyaboaouf24@gmail.com>
2024-02-22 22:01:02 +0200

    Updated NodeMCU code to communicate with server via TCP communication and initial code to handle data swapping between server and STM32

--- Robot Alpha/Alpha_ESP8266/Alpha_ESP8266.ino
+++ Robot Alpha/Alpha_ESP8266/Alpha_ESP8266.ino
@@ -1,53 +1,137 @@
 #include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
+#include <SoftwareSerial.h>
 
-const char* ssid = "pain";
-const char* password = "pain1234";
-const char* serverUrl = "http://192.168.1.4/my-api/gate.php";
-const int ledPin = 5;  // Change this to the appropriate GPIO pin
+#define RX_PIN 13  // Define RX pin
+#define TX_PIN 15  // Define TX pin
+
+SoftwareSerial espSerial(RX_PIN, TX_PIN); // Create a SoftwareSerial object
+
+const char* ssid = "Hamdy";
+const char* password = "Hamdy24@wifi";
+const char* serverIP = "192.168.1.6"; // IP address of your local server
+const int serverPort = 8001;
+
+WiFiClient client;
+// #define SERIAL_PORT Serial1 // Assuming your device is connected to Serial1
+
+bool sequenceDetected = false;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  Serial.begin(115200);
+    Serial.begin(115200);
+    espSerial.begin(115200); // Initialize Serial communication with your device
 
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
     delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
-  Serial.println("Connected to WiFi");
+
+    if (client.connect(serverIP, serverPort)) {
+        // Serial.println("Connected to server");
+        // Client will receive data from server
+        while (client.connected() && !sequenceDetected) {
+            while (client.available() > 0) {
+              // while (!sequenceDetected) {
+                // Read data from the server
+                String dataFromServer = client.readStringUntil(0xff);
+                // Send data to the device via Serial
+                 espSerial.write("dataFromServer");
+                 espSerial.print(dataFromServer);
+
+                // Wait for response from the device
+                while (espSerial.available() == 0) {
+                    delay(10);
+                }
+                // Read response from the device
+                if (espSerial.available()) { // Check if data is available from STM32 Bluepill
+                    while (espSerial.available()) {
+                      int responseFromDevice = espSerial.read(); // Read data from STM32 Bluepill
+                       // Send response to the server
+                      client.println(responseFromDevice);
+                    }
+                }
+                // String responseFromDevice = (const char*)SERIAL_PORT.read();
+               
+
+            }
+        }
+        client.stop();
+    } else {
+        // Serial.println("Connection failed");
+    }
 }
 
 void loop() {
-  if (checkGateStatus()) {
-    digitalWrite(ledPin, HIGH);  // Turn on the LED
-    delay(5000);                 // Keep the LED on for 5 seconds
-    digitalWrite(ledPin, LOW);   // Turn off the LED
-  }
-  delay(500);  // Delay between requests
+    // Not used in this example
 }
 
-bool checkGateStatus() {
-  WiFiClient client;
-  HTTPClient http;
-  // Create the URL with the action parameter
-  String url = String(serverUrl) + "?action=gate1";
 
-  // Send GET request to the server
-  if (http.begin(client, url)) {
-    int httpResponseCode = http.GET();
+// #include <ESP8266WiFi.h>
+// #include <ESP8266HTTPClient.h>
 
-    if (httpResponseCode == HTTP_CODE_OK) {
-      String response = http.getString();
-      Serial.println("Response: " + response);
+// const char* ssid = "pain";
+// const char* password = "pain1234";
+// const char* serverUrl = "http://192.168.1.4/my-api/gate.php";
+// const int ledPin = 2;  // Change this to the appropriate GPIO pin
 
-      if (response == "true") {
+// uint8_t temp=0;
+
+
+// void setup() {
+//   pinMode(ledPin, OUTPUT);
+//   Serial.begin(115200);
+//   // // Connect to Wi-Fi
+//   // WiFi.begin(ssid, password);
+//   // while (WiFi.status() != WL_CONNECTED) {
+//   //   delay(1000);
+//   //   Serial.println("Connecting to WiFi...");
+//   // }
+//   // Serial.println("Connected to WiFi");
+// }
+
+// void loop() {
+
+//   temp = Serial.read();
+//   if((temp != -1 ) && (temp != 255)){
+//     Serial.print((char)temp);
+//   }
+
+//   // if (checkGateStatus()) {
+//     // digitalWrite(ledPin, HIGH);  // Turn on the LED
+//     // delay(1000);                 // Keep the LED on for 5 seconds
+//     // digitalWrite(ledPin, LOW);   // Turn off the LED
+//     // delay(1000); 
+//   // }
+//   // delay(500);  // Delay between requests 
+// }
+
+// bool checkGateStatus() {
+//   WiFiClient client;
+//   HTTPClient http;
+//   // Create the URL with the action parameter
+//   String url = String(serverUrl) + "?action=gate1";
+
+//   // Send GET request to the server
+//   if (http.begin(client, url)) {
+//     int httpResponseCode = http.GET();
+
+//     if (httpResponseCode == HTTP_CODE_OK) {
+//       String response = http.getString();
+//       Serial.println("Response: " + response);
+
+//       if (response == "true") {
         
-        return true;
-      }
-    }
-    http.end();
-  }
-  return false;
-}
+//         return true;
+//       }
+//     }
+//     http.end();
+//   }
+//   return false;
+// }
