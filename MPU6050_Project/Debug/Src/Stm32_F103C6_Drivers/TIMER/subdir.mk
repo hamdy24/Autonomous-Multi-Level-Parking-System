4c31a6f02b3a1bb2dcdc32bd7f678f9fc277d173
Abdalla Elbahrawy <abdallabahrawyy@gmail.com>
2024-03-06 22:07:16 +0200

    MPU6050 Project uploaded

---
+++ MPU6050_Project/Debug/Src/Stm32_F103C6_Drivers/TIMER/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/Stm32_F103C6_Drivers/TIMER/Stm32_F103C6_TIMER_driver.c 
+
+OBJS += \
+./Src/Stm32_F103C6_Drivers/TIMER/Stm32_F103C6_TIMER_driver.o 
+
+C_DEPS += \
+./Src/Stm32_F103C6_Drivers/TIMER/Stm32_F103C6_TIMER_driver.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/Stm32_F103C6_Drivers/TIMER/%.o Src/Stm32_F103C6_Drivers/TIMER/%.su Src/Stm32_F103C6_Drivers/TIMER/%.cyclo: ../Src/Stm32_F103C6_Drivers/TIMER/%.c Src/Stm32_F103C6_Drivers/TIMER/subdir.mk
+	arm-none-eabi-gcc -gdwarf-2 "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F1 -DSTM32F103C8Tx -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Src-2f-Stm32_F103C6_Drivers-2f-TIMER
+
+clean-Src-2f-Stm32_F103C6_Drivers-2f-TIMER:
+	-$(RM) ./Src/Stm32_F103C6_Drivers/TIMER/Stm32_F103C6_TIMER_driver.cyclo ./Src/Stm32_F103C6_Drivers/TIMER/Stm32_F103C6_TIMER_driver.d ./Src/Stm32_F103C6_Drivers/TIMER/Stm32_F103C6_TIMER_driver.o ./Src/Stm32_F103C6_Drivers/TIMER/Stm32_F103C6_TIMER_driver.su
+
+.PHONY: clean-Src-2f-Stm32_F103C6_Drivers-2f-TIMER
+
---
+++ MPU6050_Project/Src/HAL/DC_Motor/DC_Motor.c
@@ -0,0 +1,96 @@
+/*
+ * DC_Motor.c
+ *
+ *  Author		: Hamdy Aouf
+ *  Created on	: Nov 26, 2023
+ *	Brief		: This file contains all the
+ */
+
+
+/************************************ Includes Start *************************************************/
+/************************************ Includes END  *************************************************/
+
+
+/********************************************************************/
+/*	Description:	This Function sets  		*/
+/*	Parameters:							                       		*/
+/*		@param1: 						               		*/
+/*		&param2:			       		*/
+/*	Return:			Returns Error State 	                       	*/
+/*	Notes:			None  	*/
+/********************************************************************/
+
+
+
+#include "../includes/DC_Motor.h"
+
+
+void  Motor_intialize(Motor_Config_t *Motor){
+	GPIO_PinConfig_t Motor_IN1={
+			.GPIO_MODE=GPIO_MODE_OUTPUT_PUSHPULL,
+			.GPIO_PinNumber=Motor->DC_Pin1Number,
+			.GPIO_OUTPUT_SPEED=GPIO_SPEED_2MHZ
+	};
+	MCAL_GPIO_Init(Motor->DC_PortNumber, &Motor_IN1);
+
+	GPIO_PinConfig_t Motor_IN2={
+			.GPIO_MODE=GPIO_MODE_OUTPUT_PUSHPULL,
+			.GPIO_PinNumber=Motor->DC_Pin2Number,
+			.GPIO_OUTPUT_SPEED=GPIO_SPEED_2MHZ
+	};
+	MCAL_GPIO_Init(Motor->DC_PortNumber, &Motor_IN2);
+
+
+//	GPIO_PinConfig_t DC_En2Pin = { .GPIO_PinNumber = GPIO_PIN_6, .GPIO_MODE =
+//			GPIO_MODE_OUTPUT_PUSHPULL, .GPIO_OUTPUT_SPEED = GPIO_SPEED_2MHZ };
+//	MCAL_GPIO_Init(GPIOB, &DC_En2Pin);
+//
+//	GPIO_PinConfig_t DC_En1Pin = { .GPIO_PinNumber = GPIO_PIN_7, .GPIO_MODE =
+//			GPIO_MODE_OUTPUT_PUSHPULL, .GPIO_OUTPUT_SPEED = GPIO_SPEED_2MHZ };
+//	MCAL_GPIO_Init(GPIOB, &DC_En1Pin);
+
+
+//	MCAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_LOW);
+//	MCAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_LOW);
+
+}
+
+
+
+
+
+void Motor_TurnOff(Motor_Config_t *Motor){
+	MCAL_GPIO_WritePin(Motor->DC_PortNumber, Motor->DC_Pin1Number, GPIO_PIN_LOW);
+	MCAL_GPIO_WritePin(Motor->DC_PortNumber, Motor->DC_Pin2Number, GPIO_PIN_LOW);
+	MCAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_LOW);
+	MCAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_LOW);
+
+	TIMER4_Stop();
+}
+
+
+
+
+void Motor_Move_ForWard(Motor_Config_t *Motor, uint8_t speed){
+//
+//
+//	MCAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_HIGH);
+//	MCAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_HIGH);
+
+	PWM(Motor->PWM_Timer, Motor->PWM_Channel, speed, 1000, RCC_CLK_8M);
+
+	MCAL_GPIO_WritePin(Motor->DC_PortNumber, Motor->DC_Pin1Number,GPIO_PIN_HIGH);
+	MCAL_GPIO_WritePin(Motor->DC_PortNumber, Motor->DC_Pin2Number,GPIO_PIN_LOW);
+
+}
+
+
+
+
+void Motor_Move_BackWard(Motor_Config_t *Motor, uint8_t speed){
+	PWM(Motor->PWM_Timer, Motor->PWM_Channel, speed, 1000, RCC_CLK_8M);
+	MCAL_GPIO_WritePin(Motor->DC_PortNumber, Motor->DC_Pin1Number,GPIO_PIN_LOW);
+	MCAL_GPIO_WritePin(Motor->DC_PortNumber, Motor->DC_Pin2Number,GPIO_PIN_HIGH);
+}
+
+
