bfce7726a8336fe73af465bf4b9dc15d1319590e
hamdy24 <hamdyaboaouf24@gmail.com>
2024-07-20 15:11:03 +0300

    Final Server update working and fully tested

--- Tests/Robot Alpha/STM32F301/Debug/Src/HAL/DC_Motor/DC_Motor.d
+++
@@ -1,7 +0,0 @@
-Src/HAL/DC_Motor/DC_Motor.o: ../Src/HAL/DC_Motor/DC_Motor.c \
- ../Src/HAL/DC_Motor/DC_Motor.h \
- ../Src/HAL/DC_Motor/../../Stm32_F103C6_Drivers/inc/Stm32_F103C6_gpio_driver.h \
- ../Src/HAL/DC_Motor/../../Stm32_F103C6_Drivers/inc/stm32f103c6.h
-../Src/HAL/DC_Motor/DC_Motor.h:
-../Src/HAL/DC_Motor/../../Stm32_F103C6_Drivers/inc/Stm32_F103C6_gpio_driver.h:
-../Src/HAL/DC_Motor/../../Stm32_F103C6_Drivers/inc/stm32f103c6.h:
